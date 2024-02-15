164472aa81d5dcb371c759cd20658388df7f0991
Daniel Carvalho <Danielscg2012@gmail.com>
2024-02-15 11:24:55 -0300

    add esqueleto do projeto

--- Front-end/pacPay/src/app/app.component.ts
+++ Front-end/pacPay/src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { CadastroComponent } from './pages/cadastro/cadastro.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    HomeComponent,
+    LoginComponent,
+    CadastroComponent,
+    HeaderComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'pacPay';
---
+++ Front-end/pacPay/src/app/pages/cadastro/cadastro.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-cadastro',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './cadastro.component.html',
+  styleUrl: './cadastro.component.css',
+})
+export class CadastroComponent {
+  constructor(private rota: Router) {}
+
+  formulario = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    senha: new FormControl('', Validators.required),
+  });
+
+  cadastrar(): void {
+    localStorage.setItem('email', this.formulario.value.email || '');
+    localStorage.setItem('senha', this.formulario.value.senha || '');
+
+    this.rota.navigateByUrl('/login');
+  }
+}

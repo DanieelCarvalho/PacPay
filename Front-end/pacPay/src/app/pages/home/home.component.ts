ce355fc016cd89423d476b6a6109bf91bc2a92ab
Daniel Carvalho <Danielscg2012@gmail.com>
2024-02-18 10:34:31 -0300

    add cartão com css

--- Front-end/pacPay/src/app/pages/home/home.component.ts
+++ Front-end/pacPay/src/app/pages/home/home.component.ts
@@ -4,17 +4,19 @@ import { LoginComponent } from '../login/login.component';
 import { Router } from '@angular/router';
 import { HeaderComponent } from '../../components/header/header.component';
 import { FooterComponent } from '../../components/footer/footer.component';
+import { CartaoComponent } from '../../components/cartao/cartao.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
-  imports: [RouterLink, LoginComponent, HeaderComponent, FooterComponent],
+  imports: [
+    RouterLink,
+    LoginComponent,
+    HeaderComponent,
+    FooterComponent,
+    CartaoComponent,
+  ],
 })
-export class HomeComponent {
-  // constructor(private router: Router) {}
-  // cadastro(): void {
-  //   this.router.navigate(['/cadastro']);
-  // }
-}
+export class HomeComponent {}

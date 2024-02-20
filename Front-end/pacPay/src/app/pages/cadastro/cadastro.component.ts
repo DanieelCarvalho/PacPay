4d49b1379549ca23a7b9d00a0fb2383189fe0f19
Luan Charlys <firminocharlys@gmail.com>
2024-02-20 06:46:44 -0300

    feat: validacao da senha

--- Front-end/pacPay/src/app/pages/cadastro/cadastro.component.ts
+++ Front-end/pacPay/src/app/pages/cadastro/cadastro.component.ts
@@ -8,27 +8,67 @@ import {
 } from '@angular/forms';
 import { Router } from '@angular/router';
 
+import { NgIf } from '@angular/common';
+
 @Component({
   selector: 'app-cadastro',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, NgIf],
   templateUrl: './cadastro.component.html',
   styleUrls: ['./cadastro.component.css'],
 })
 export class CadastroComponent {
   isFocus = false;
+  value: unknown;
 
   constructor(private rota: Router) {}
 
   formulario = new FormGroup({
+    nome: new FormControl('', Validators.required),
+    cpf: new FormControl('', Validators.required),
+    dataNascimento: new FormControl('', Validators.required),
+    telefone: new FormControl('', Validators.required),
     email: new FormControl('', [Validators.required, Validators.email]),
-    senha: new FormControl('', Validators.required),
+    senha: new FormControl('', [Validators.required. validators.minLength(8)], Validators.maxLength(16), Validators.pattern(/^(?=.*[a-zA-Z])(?=.*\d)(?=.*[^a-zA-Z0-9]).{8,16}$/)),
+    cep: new FormControl('', Validators.required),
+    rua: new FormControl('', Validators.required),
+    numero: new FormControl('', Validators.required),
+    complemento: new FormControl('', Validators.required),
+    bairro: new FormControl('', Validators.required),
+    cidade: new FormControl('', Validators.required),
+    referencia: new FormControl('', Validators.required),
   });
 
   cadastrar(): void {
-    localStorage.setItem('email', this.formulario.value.email ?? '');
-    localStorage.setItem('senha', this.formulario.value.senha ?? '');
+    if (this.formulario.valid) {
+      console.log(this.formulario.value.email);
+      console.log(this.formulario.value.senha);
+      console.log(this.formulario.value);
+    } else {
+      Object.keys(this.formulario.controls).forEach((field) => {
+        const control = this.formulario.get(field);
+        if (control) {
+          control.markAsTouched({ onlySelf: true });
+        }
+      });
+    }
+  }
 
-    this.rota.navigateByUrl('/login');
+  validarSenha(event: Event): boolean {
+    const value = (event.target as HTMLInputElement).value;
+    if (value === null) return false;
+
+    if (value.length < 8 || value.length > 16) return false;
+
+    const letras = value.match(/[a-zA-Z]/g);
+    if (!letras || letras.length < 2) return false;
+
+    const numeros = value.match(/\d/g);
+    if (!numeros || numeros.length < 2) return false;
+
+    const especiais = value.match(/[^a-zA-Z0-9]/g);
+    if (!especiais || especiais.length < 1) return false;
+
+    return true;
   }
 }
